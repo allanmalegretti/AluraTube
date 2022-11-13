4ec6f5bb02707986630df6d22c43267b6f25a246
Allan M Alegretti <allanalegretti@gmail.com>
2022-11-13 12:51:20 -0300

    Botao para formulario de adicionar video

--- src/components/Menu/components/RegisterVideo/index.js
+++ src/components/Menu/components/RegisterVideo/index.js
@@ -3,9 +3,9 @@ import { StyledRegisterVideo } from "./Styles";
 
 export default function RegisterVideo() {
     const [formVisivel, setFormVisivel] = React.useState(false);
-    const [values, setValues] = React.useState({ titulo: "" , url: "" });
+    const [values, setValues] = React.useState({ titulo: "", url: "" });
 
-    console.log(formVisivel);
+    // console.log(formVisivel);
 
     return (
         <StyledRegisterVideo>
@@ -15,13 +15,34 @@ export default function RegisterVideo() {
 
             {formVisivel
                 ? (
-                    <form>
+                    <form onSubmit={(evento) => {
+                        evento.preventDefault();
+                        console.log(values);
+                    }}>
                         <div>
                             <button className="close-modal" onClick={() => setFormVisivel(false)}>
                                 x
                             </button>
-                            <input placeholder="Título do Vídeo" />
-                            <input placeholder="URL" />
+                            <input placeholder="Título do Vídeo"
+                                value={values.titulo}
+                                onChange={(evento) => {
+                                    const value = evento.target.value;
+                                    console.log(value);
+                                    setValues({
+                                        ...values,
+                                        titulo: value,
+                                    });
+                                }} />
+                            <input placeholder="URL"
+                                value={values.url}
+                                onChange={(evento) => {
+                                    const value = evento.target.value;
+                                    console.log(value);
+                                    setValues({
+                                        ...values,
+                                        url: value,
+                                    });
+                                }} />
                             <button type="submit">
                                 Cadastrar
                             </button>
